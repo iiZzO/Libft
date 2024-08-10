1819d8b0af261b6b935542168abb04352a40a619
iiZzO <ismachiloeches@gmail.com>
2024-08-10 20:15:56 +0200

    Revisar nombre funcion + coms

---
+++ ft_Istadd_front.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_Istadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismael <ismael@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 20:13:28 by ismael            #+#    #+#             */
+/*   Updated: 2024/08/10 20:14:44 by ismael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	temp = lst[0];
+	lst[0] = new;
+	new->next = temp;
+}
+
+/*
+int main()
+{
+	t_list *list = ft_lstnew("test1");
+	list->next = ft_lstnew("test2");
+
+	t_list *new = ft_lstnew("new");
+	ft_lstadd_front(&list, new);
+	printf("%d\n", ft_lstsize(list));
+}
+*/
---
+++ ft_Istclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_Istclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismael <ismael@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 20:15:00 by ismael            #+#    #+#             */
+/*   Updated: 2024/08/10 20:15:40 by ismael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*current;
+
+	if (lst == 0 || del == 0)
+		return ;
+	while (*lst != 0)
+	{
+		current = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = current;
+	}
+}
