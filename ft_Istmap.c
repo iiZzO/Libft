24032c969c6eab35341b5cf2a7597be6e6c866da
iiZzO <ismachiloeches@gmail.com>
2024-08-10 20:38:10 +0200

    Revisar nombre

---
+++ ft_Istmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_Istmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismael <ismael@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 20:37:38 by ismael            #+#    #+#             */
+/*   Updated: 2024/08/10 20:37:57 by ismael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*first;
+
+	if (lst == 0 || f == 0 || del == 0)
+		return (0);
+	first = 0;
+	while (lst != 0)
+	{
+		new = ft_lstnew((*f)(lst->content));
+		if (new == 0)
+		{
+			ft_lstclear(&first, del);
+			return (first);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	new->next = 0;
+	return (first);
+}
