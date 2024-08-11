6de5de833b80b9af1107a6841e172567438193ee
iiZzO <ismachiloeches@gmail.com>
2024-08-11 03:45:26 +0200

    Chek main

---
+++ ft_strlcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismael <ismael@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 03:44:59 by ismael            #+#    #+#             */
+/*   Updated: 2024/08/11 03:45:19 by ismael           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (ft_strlen(src));
+	while (src[i] != '\0' && i < (n - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
+
+/*
+int	main()
+{
+	char dest[30];
+
+	ft_strlcpy(dest, "hola que tal", 13);
+	printf("%s\n", dest);
+}
+*/
